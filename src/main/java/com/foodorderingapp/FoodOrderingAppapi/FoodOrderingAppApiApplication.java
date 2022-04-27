2b3791d02586e254d01f06f0a395785deb9c7ee2
kiruthikakavi01 <kiruthikakavi01@gmail.com>
2022-04-27 10:16:12 +0530

    added features

---
+++ src/main/java/com/foodorderingapp/FoodOrderingAppapi/FoodOrderingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodorderingapp.FoodOrderingAppapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingAppApiApplication.class, args);
+	}
+
+}
